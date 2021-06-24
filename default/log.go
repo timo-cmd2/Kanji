d2e095fc3c268e69eb53afd92e9860ac0d4e6bf1
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-06-24 15:48:47 +0200

    Create log.go

---
+++ default/log.go
@@ -0,0 +1,13 @@
+package default
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/mattn/go-colorable"
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
